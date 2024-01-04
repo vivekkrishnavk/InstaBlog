6a6a0de0d19f5d25c5bcf6a72afbd0ec91a19219
Vivek Krishna <vivekkrishnavk@gmail.com>
2024-01-04 20:07:36 +0530

    modified site meta data

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,7 +1,7 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
+  title: "Instarem's Engineering Blog",
+  author: 'Vivek Krishna',
   headerTitle: 'TailwindBlog',
   description: 'Engineering Excellence | SRE Best Practices | Dev Optimizations',
   language: 'en-us',
