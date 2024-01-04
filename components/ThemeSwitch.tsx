3a13d46113e07c2cca6e6600cb444901849463ee
Vivek Krishna <vivekkrishnavk@gmail.com>
2024-01-04 20:00:08 +0530

    added the theme switcher back

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -8,7 +8,10 @@ const ThemeSwitch = () => {
   const { theme, setTheme, resolvedTheme } = useTheme()
 
   // When mounted on client, now we can show the UI
-  useEffect(() => setMounted(true), [])
+  useEffect(() => {
+    setMounted(true)
+    setTheme('light')
+  }, [])
 
   if (!mounted) {
     return null
